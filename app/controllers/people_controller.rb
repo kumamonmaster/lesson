99522befa822d792104e1cc3e9d457c7d2078e12
Takuma <fuusuke0715@gmail.com>
2016-09-08 05:37:15 +0000

    Add step2

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,8 @@ class PeopleController < ApplicationController
   def step1
     @person = Person.find(1)
   end
+
+  def step2
+    @people = Person.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/step1' => 'people#step1'
+  get '/step2' => 'people#step2'
 end
